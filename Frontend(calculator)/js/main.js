3aaa3976b98ea0dc8217ba380d303e73a79b8d3a
ArslanGeek <bra.kreedz@gmail.com>
2023-05-04 17:02:59 +0600

    Add files via upload

---
+++ Frontend(calculator)/js/main.js
@@ -0,0 +1,32 @@
+//переменная, в которой хранятся наши математические действия
+
+
+let op
+//функция для результата
+function calc() {
+    let result
+    let num1 = Number(document.getElementById('num1').value)
+    let num2 = Number(document.getElementById('num2').value)
+    switch (op) {
+        case '+':
+            result = num1 + num2
+            break;
+        case '-':
+            result = num1 - num2
+            break;
+        case '*':
+            result = num1 * num2
+            break;
+        case '/':
+            if (num2 === 0) {
+                document.getElementById('result').textContent = 'ошибка'
+                    return
+            }
+            result = num1 / num2
+            break
+        default:
+            document.getElementById('result')
+                .textContent = 'ошибка, выберите действие'
+    }
+    document.getElementById('result').innerText = result
+}
